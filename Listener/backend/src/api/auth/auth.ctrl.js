863a28a225d518f6270175af0838b9df7ed1bc37
coolkim99 <coolkim99@sookmyung.ac.kr>
2023-06-18 19:16:34 +0900

    deploy

--- Listener/backend/src/api/auth/auth.ctrl.js
+++ Listener/backend/src/api/auth/auth.ctrl.js
@@ -93,6 +93,7 @@ export const login = async ctx => {
 */
 export const check = async ctx => {
   const { user } = ctx.state;
+  console.log(user);
   if (!user) {
     // 로그인중 아님
     ctx.status = 401; // Unauthorized
